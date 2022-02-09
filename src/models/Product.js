55d3e521f2f2a949f4b20f750164fcbe2f1a0e66
Loc <ginga550504@gmail.com>
2022-02-09 19:54:04 +0700

    update

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,15 +1,23 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let ObjectId = mongoose.Types.ObjectId;
 
-let ProductSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  price: { type: Number, required: true, unique: true },
-  thumbnail: { type: String },
-  num: { type: Number },
-  key_product: String,
-  category_id: { type: ObjectId, ref: "Category" },
-  delete: Boolean,
-}, { timestamps: true });
+let ProductSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    price: { type: Number, required: true, unique: true },
+    num: { type: Number },
+    key_product: String,
+    disc: String,
+    thumbnail: { type: String },
+    detail: { type: String },
+    rate: { type: Number },
+    view: Number,
+    delete: Boolean,
+    category_id: { type: ObjectId, ref: "Category" },
+    discount_id: { type: ObjectId, ref: "Discount" },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model("Product", ProductSchema);
--- src/server.js
+++ src/server.js
@@ -1,26 +1,53 @@
 import bodyParser from "body-parser";
+import multiparty from "connect-multiparty";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import fs from "fs";
 import mongoose from "mongoose";
+import path from "path";
 import initWebRoutes from "./routers/web";
+import multer from "multer";
 
 dotenv.config();
 let app = express();
 const PORT = process.env.PORT || 5000;
 // const BASE_URL = process.env.BASE_URL_CLIENT;
+
 const BASE_URL =
   "https://61f8efa17bcc72792bf5586b--vigorous-stonebraker-6e0dce.netlify.app";
 const URI_DB = process.env.URI_DB;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/asset/upload/");
+  },
+  filename: (req, file, cb) => {
+    let ext = file.originalname.split(".");
+    ext = ext[ext.length - 1];
+    cb(null, `${Date.now()}.${ext}`);
+  },
+});
+const upload = multer({ storage: storage });
 
 mongoose
   .connect(URI_DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB Connection Successfully!"))
   .catch((err) => console.log(err));
 
+app.use(express.static(__dirname + "/asset/upload"));
 app.use(cors({ credentials: true, origin: "*" }));
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
+app.use(upload.array("files"));
+
+app.post("/api/admin/upload", (req, res) => {
+  const tempFile = req.files;
+
+  if (tempFile.length > 0) {
+    res.json(req.files[0]);
+  }
+});
 initWebRoutes(app);
 
 app.listen(PORT, () =>
